e77ff8f43e6a714a629477b7092a3713995aaeac
Nil Díaz <nildiazb@gmail.com>
2026-05-17 11:24:47 +0200

    refactor: clean up and reorganize product feature documentation files and metadata

--- apps/docs/sync-docs.sh
+++ apps/docs/sync-docs.sh
@@ -9,6 +9,7 @@ find "$PAGES_DIR" -mindepth 1 -maxdepth 1 -not -name "_app.tsx" -not -name "_doc
 
 # Since the root /docs already follows the professional structure:
 cp -r ../../docs/* "$PAGES_DIR/"
+rm -rf "$PAGES_DIR/product"
 
 # Use the root README.md as the main landing page
 cp ../../README.md "$PAGES_DIR/index.md"
